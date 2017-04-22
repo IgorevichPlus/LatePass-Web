70eb1b9092173c4f12999c2e92ed725f2ada0e26
David Shustin <david.shustin@gmail.com>
2017-04-22 13:24:28 -0400

    Add basic log in functionality

---
+++ js/index.js
@@ -0,0 +1,28 @@
+var logIn = function() {
+	// alert("f");
+	let username = document.getElementById("username"), password = document.getElementById("password");
+	// alert(username.val);
+	firebase.auth().signInWithEmailAndPassword(username.value, password.value).catch(function(error) {
+		let errCode = error.code;
+		let errMsg = error.message;
+		let content;
+		switch(errCode) {
+			case 'auth/invalid-email':
+				content = 'The username is not a valid email.';
+				break;
+			case 'auth/user-disabled':
+				content = 'Your account is disabled.';
+				break;
+			case 'auth/user-not-found':
+				content = 'This user does not exist.';
+				break;
+			case 'auth/wrong-password':
+				content = 'This password is incorrect.';
+				break;
+			default:
+				content = errMsg;
+				break;
+		}
+		document.getElementById("error").innerHTML = content;
+	})
+}
